a7a3299f69c70d7f28463aeaa1e4f4be5718982e
Leonardo Milleri <lmilleri@redhat.com>
2024-01-15 13:54:11 +0000

    more scripts

--- vhost-vdpa/get-vhost-pod-info.sh
+++ vhost-vdpa/get-vhost-pod-info.sh
@@ -3,3 +3,4 @@ oc exec -it -n vdpa vdpa-pod711 -- env | grep vhost-vdpa
 echo "POD712:"
 oc exec -it -n vdpa vdpa-pod712 -- env | grep vhost-vdpa
 oc exec -it -n openshift-ovn-kubernetes `./find-ovnkube-node-711.sh | awk 'FNR == 2 {print $1}'` -c ovnkube-controller -- /usr/bin/ovn-nbctl show l2.network_ovn_localnet_switch
+oc exec -it -n openshift-ovn-kubernetes `./find-ovnkube-node-712.sh | awk 'FNR == 2 {print $1}'` -c ovnkube-controller -- /usr/bin/ovn-nbctl show l2.network_ovn_localnet_switch
--- vhost-vdpa/label-nodes.sh
+++ vhost-vdpa/label-nodes.sh
@@ -2,3 +2,5 @@ oc label node virtlab711.virt.lab.eng.bos.redhat.com node-role.kubernetes.io/mcp
 oc label node virtlab712.virt.lab.eng.bos.redhat.com node-role.kubernetes.io/mcp-offloading=""
 oc label node virtlab711.virt.lab.eng.bos.redhat.com feature.node.kubernetes.io/network-sriov.capable=true
 oc label node virtlab712.virt.lab.eng.bos.redhat.com feature.node.kubernetes.io/network-sriov.capable=true
+oc label node virtlab712.virt.lab.eng.bos.redhat.com region="712"
+oc label node virtlab711.virt.lab.eng.bos.redhat.com region="711"
